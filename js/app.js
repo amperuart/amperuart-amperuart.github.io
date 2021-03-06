6048b6c06c0e5abd52a63c66b96b44e591f0261e
Alejandra Villanueva Zapata <contacto@amperu.art>
2021-03-06 10:27:49 -0500

    Readme html css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,53 @@
+// Constantes
+const listaMascotas = document.querySelector(".listaMascotas");
+const mascotas = document.getElementById("nombre");
+
+// Listeners
+initApp();
+
+document.addEventListener("load", initAPP)
+
+// Funciones
+function initApp() {
+    // console.log("Hola, mundo");
+
+    listaMascotas.addEventListener("click", obtenerMascota)
+}
+
+function obtenerMascota(e) {
+
+    if(e.target.classList.contains("agregarCarrito")) {
+        const curso = e.target.parentElement;
+
+        extraerDatos(mascota);
+    }
+
+}
+
+function extraerDatos(mascota) {
+    const mascotaElegida = {
+        nombre: mascota.querySelector("h2").textContent,
+    }
+
+    // console.log(mascotaElegida)
+    pintarMascota(mascotaElegida)
+}
+
+function pintarMascota(mascota) {
+    let container = document.createElement("div");
+    let nombre = document.createElement("p");
+    let precio = document.createElement("p");
+
+    nombre = mascota.nombre;
+
+    console.log(nombre);
+
+    cursos.appendChild(container)
+}
+
+
+
+
 /*
 	Add to cart fly effect with jQuery. - May 05, 2013
 	(c) 2013 @ElmahdiMahmoud - fikra-masri.by
--- readme.md
+++ readme.md
@@ -2,9 +2,7 @@
     Porque generará codigo difícil de leer y posibles consecuencias.
 
 ¿Qué es una promesa?
-    Una Promise (promesa en castellano) es un objeto que representa la terminación o el fracaso eventual de una operación asíncrona.Esto quiere decir que podremos saber cuando un llamado asíncrono se resolvió o se rechazó.
-    
-    Para crear una promesa se hace a través del objeto Promise, Este recibe como parámetro una función callback, que retorna la función resolve y la función reject .
+    Una Promise (promesa en castellano) es un objeto que representa la terminación o el fracaso eventual de una operación asíncrona.Esto quiere decir que podremos saber cuando un llamado asíncrono se resolvió o se rechazó. Para crear una promesa se hace a través del objeto Promise, Este recibe como parámetro una función callback, que retorna la función resolve y la función reject .
 
 ¿Qué es el callback hell?
     Este enfoque de usar callbacks es perfecto cuando solo tenemos que resolver un llamado asíncrono. Si se llaman 3 funciones asíncronas que dependen una de otra se anidan y esto nos genera un infierno de callbacks.
