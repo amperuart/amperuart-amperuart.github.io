c63832911908272283a46f1f654ce430e8eefade
Alejandra Villanueva Zapata <contacto@amperu.art>
2021-03-07 12:47:34 -0500

    Cambio de Json

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // Get request
-  fetch("mascotas.json")
-    .then( response => response.json() )
-    .then( data => console.log(data) )
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(data => console.log(data));
 
 // Post request
-  fetch("mascotas.json/posts", {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "Post",
     body: JSON.stringify({fakeData: 1}),
     headers: {
@@ -15,7 +15,7 @@
     .then(data => console.log(data));
 
 // Put request
-  fetch("mascotas.json/posts/1", {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
     method: "Put",
     body: JSON.stringify({fakeData: 200, id: 1}),
     headers: {
@@ -26,7 +26,7 @@
     .then(data => console.log(data));
 
 // Delete request
-  fetch("mascotas.json/posts/1", {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
     method: "Delete"
   })
     .then(response => response.json())
